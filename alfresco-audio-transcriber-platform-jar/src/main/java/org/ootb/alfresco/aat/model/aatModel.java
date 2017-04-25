d09422f7a02885223a9d5c61515fa8e24bbeae8a
Mikel Asla <mikel.asla@keensoft.es>
2017-04-25 14:30:30 +0200

    Initial commit in beecon2017 branch

---
+++ alfresco-audio-transcriber-platform-jar/src/main/java/org/ootb/alfresco/aat/model/aatModel.java
@@ -0,0 +1,27 @@
+package org.ootb.alfresco.aat.model;
+
+import org.alfresco.service.namespace.QName;
+
+public interface aatModel {
+
+	/*
+	 * Namespace model
+	 */
+	public static final String NAMESPACE_AAT_CONTENT_MODEL = "http://www.fegorsoft.com/model/aat/1.0";
+
+	/*
+	 * Aspects
+	 */
+	public static final QName ASPECT_TRANSCRIBER = QName.createQName(
+			NAMESPACE_AAT_CONTENT_MODEL, "Transcriber");
+
+	/*
+	 * Properties
+	 */
+	public static final QName PROP_HYPHOTESIS = QName.createQName(
+			NAMESPACE_AAT_CONTENT_MODEL, "Hyphotesis");	
+	public static final QName PROP_WORDS = QName.createQName(
+			NAMESPACE_AAT_CONTENT_MODEL, "Words");
+	public static final QName PROP_FRAMES = QName.createQName(
+			NAMESPACE_AAT_CONTENT_MODEL, "Frames");	
+}
